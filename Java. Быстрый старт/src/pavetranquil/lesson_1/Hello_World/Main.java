646ac869f8c137f2b848bcd250fc2a48a9f40a30
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2017-05-01 00:12:29 +0300

    Delete "Java. Быстрый старт"
    Курс "Java. Быстрый старт" не подлежит восстановлению и он удалён...

--- Java. Быстрый старт/src/pavetranquil/lesson_1/Hello_World/Main.java
+++
@@ -1,9 +0,0 @@
-package pavetranquil.lesson_1.Hello_World;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Hello, World!");
-        System.out.println("Я буду изучать Java!");
-    }
-}
