ba35f8bb2dde911492cfdf6c43650cdb66b2eda3
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2017-05-07 00:21:28 +0300

    Создание персон (объектов, классов)

---
+++ Python. ООП/Создание персон/Main.py
@@ -0,0 +1,20 @@
+from Person import Person
+
+man = Person()
+
+man.descriptionOfPerson()
+
+
+man.setName("Vasya")
+man.setAge("14")
+
+man.descriptionOfPerson()
+
+
+
+woman = Person()
+
+woman.setName("Alisa")
+woman.setAge("15")
+
+woman.descriptionOfPerson()
