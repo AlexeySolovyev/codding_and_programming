9f6408dca010d580ad0af3a5cf6a20bc9a3f2c96
PaveTranquil <pasha.2ovchinnikov@gmail.com>
2018-09-03 22:33:13 +0300

    Перезапуск проектов PTCoddng - 1 часть
    Удаление файлов от старого метода компиляции

--- Самостоятельные работы/Python/Создание exe/setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name = "Name_project",
-    version = "version",
-    description = "About project",
-    executables = [Executable("main.py")]
-)
